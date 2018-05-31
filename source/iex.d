eef1b321f77486a1461a1421b58a6e90a097d4e8
Ryan Frame <code@ryanjframe.com>
2018-05-31 12:23:25 -0400

    Begin support for the chart endpoint.

--- source/iex.d
+++ source/iex.d
@@ -32,7 +32,10 @@ enum EndpointType : string {
     VolumeByVenue = "volume-by-venue",
 }
 
-alias ParamSet = string[string];
+struct Endpoint {
+    string urlString;
+    string[string] params;
+}
 
 /** Build a query on the Stock endpoint of the IEX API.
 
@@ -57,7 +60,7 @@ struct Stock {
             }
 
             queryString ~= "&types=";
-            foreach (type, params; paramSet) {
+            foreach (type, params; this.endpoints) {
                  queryString ~= type ~ ",";
             }
             queryString = queryString[0..$-1];
@@ -65,10 +68,10 @@ struct Stock {
             queryString = "stock/" ~ symbols[0] ~ "/";
         }
 
-        if (paramSet.length == 1) {
+        if (this.endpoints.length == 1) {
             queryString ~= buildEndpoint(
-                    paramSet.keys()[0],
-                    paramSet[paramSet.keys()[0]],
+                    this.endpoints.keys()[0],
+                    this.endpoints[this.endpoints.keys()[0]],
                     this.queriesMultipleSymbols());
         } else
             assert(0, "Not implemented");
@@ -78,19 +81,37 @@ struct Stock {
 
     private:
 
-    void addQueryType(EndpointType type, string[string] params = null) {
-        this.paramSet[type] = params;
+    /** Add a query type to the Stock HTTP query.
+
+        Params:
+            type =          The type of the endpoint to add.
+            params =        Any parameters to include.
+            urlAddition =   Any necessary text to append to the endpoint.
+    */
+    void addQueryType(
+            EndpointType type,
+            string[string] params = null,
+            string urlAddition = "") {
+        Endpoint p = {
+            urlString: type ~ urlAddition,
+            params: params
+        };
+        this.endpoints[type] = p;
     }
 
-    string buildEndpoint(EndpointType type, ParamSet params, bool isContinuing = false) {
-        if (params.length == 0) {
-            return isContinuing ? "" : type;
+    string buildEndpoint(
+            EndpointType type,
+            Endpoint endpoint,
+            bool isContinuing = false) {
+
+        if (endpoint.params.length == 0) {
+            return isContinuing ? "" : endpoint.urlString;
         } else {
-            string endpoint = isContinuing ? "&" : type ~ "?";
-            foreach (param, value; params) {
-                endpoint ~= param ~ "=" ~ value ~ "&";
+            string endpointString = isContinuing ? "&" : endpoint.urlString ~ "?";
+            foreach (param, value; endpoint.params) {
+                endpointString ~= param ~ "=" ~ value ~ "&";
             }
-            return endpoint[0..$-1];
+            return endpointString[0..$-1];
         }
     }
 
@@ -98,7 +119,7 @@ struct Stock {
 
     string[] symbols;
 
-    ParamSet[EndpointType] paramSet;
+    Endpoint[EndpointType] endpoints;
 }
 
 /** Send a Query object to the IEX API and return the JSON results. */
@@ -128,21 +149,26 @@ Json query(string query, string[string] params, string prefix = iexPrefix) {
 
 @("quote() builds an endpoint for a single stock symbol")
 unittest {
+    import std.string : split;
     auto stock = Stock("AAPL").quote();
     assert(stock.toURL() == iexPrefix ~ "stock/AAPL/quote", stock.toURL());
 
     stock = Stock("AAPL").quote(true);
-    assert(stock.toURL() == iexPrefix ~ "stock/AAPL/quote?displayPercent=true",
-            stock.toURL());
+    auto actual = stock.toURL().split('?');
+    assert(actual[0] == iexPrefix ~ "stock/AAPL/quote");
+    assert(actual[1].hasParameters(["displayPercent=true"]));
 }
 
 @("quote() builds an endpoint for multiple stock symbols")
 unittest {
+    import std.string : split;
     auto stock = Stock("AAPL", "BDC").quote();
     assert(stock.toURL() == iexPrefix ~ "stock/market/batch?symbols=AAPL,BDC&types=quote", stock.toURL());
 
     stock = Stock("AAPL", "BDC").quote(true);
-    assert(stock.toURL() == iexPrefix ~ "stock/market/batch?symbols=AAPL,BDC&types=quote&displayPercent=true", stock.toURL());
+    auto actual = stock.toURL().split('?');
+    assert(actual[0] == iexPrefix ~ "stock/market/batch");
+    assert(actual[1].hasParameters(["symbols=AAPL,BDC", "types=quote", "displayPercent=true"]));
 }
 
 /** Request a quote for the stock(s).
@@ -161,3 +187,95 @@ Stock quote(Stock stock, bool displayPercent = false) {
     return stock;
 }
 
+/** Values for the date range in a chart query. A custom date can be used
+    instead.
+*/
+enum ChartRange : string {
+    FiveYears = "5y",
+    TwoYears = "2y",
+    OneYear = "1y",
+    YearToDate = "ytd",
+    YTD = ChartRange.YearToDate,
+    SixMonths = "6m",
+    ThreeMonths = "3m",
+    OneDay = "1d",
+    Dynamic = "dynamic"
+}
+
+@("chart() builds an endpoint for pre-defined date ranges")
+unittest {
+    auto stock = Stock("AAPL").chart(ChartRange.YearToDate);
+    assert(stock.toURL() == iexPrefix ~ "stock/AAPL/chart/"  ~ ChartRange.YTD,
+            stock.toURL());
+
+    stock = Stock("AAPL").chart(ChartRange.YearToDate, false, true, 3);
+
+    import std.string : split;
+    auto actual = stock.toURL().split('?');
+    assert(actual[0] == iexPrefix ~ "stock/AAPL/chart/" ~ ChartRange.YTD,
+            actual[0]);
+    assert(actual[1].hasParameters(["chartSimplify=true", "chartInterval=3"]),
+            actual[1]);
+}
+
+@("chart() builds an endpoint for custom dates")
+unittest {
+}
+
+@("chart() builds an endpoint for multiple stock symbols")
+unittest {
+}
+
+bool hasEnumMember(E, T)(T value) if (is(E == enum)) {
+    import std.traits : EnumMembers;
+    foreach (member; EnumMembers!E) {
+        if (member == value) return true;
+    }
+    return false;
+}
+
+// TODO Doc: date currently only supports last 30 days.
+Stock chart(Stock stock, string range,
+        bool resetAtMidnight = false, bool simplify = false, int interval = -1,
+        bool changeFromClose = false, int last = -1) {
+    import std.conv : text;
+    import std.string : isNumeric;
+
+    string[string] params;
+    if (! (hasEnumMember!ChartRange(range)
+            || (range.isNumeric && range.length == 8))) {
+        throw new Exception("Invalid range for chart: " ~ range);
+    }
+
+    if (resetAtMidnight) params["chartReset"] = "true";
+    if (simplify) params["chartSimplify"] = "true";
+    if (interval > 0) params["chartInterval"] = interval.text;
+    if (changeFromClose) params["changeFromClose"] = "true";
+    if (last > 0) params["chartLast"] = last.text;
+
+    stock.addQueryType(EndpointType.Chart, params, "/" ~ range);
+    return stock;
+}
+
+private:
+
+version(unittest) {
+    /**  Check that a string has the provided parameters in unittests.
+
+        Parameters in a URL may be in any order, so a simple equality check is
+        insufficient.
+    */
+    bool hasParameters(string input, string[] parameters) {
+        import std.algorithm.searching : canFind;
+        foreach (parameter; parameters) {
+            if (! canFind(input, parameter)) return false;
+        }
+        return true;
+    }
+
+    @("hasParameters() checks for substrings")
+    unittest {
+        assert("some-url?one=two&three=four".hasParameters(["one=two","three=four"]));
+        assert(! "some-url?one=two".hasParameters(["three=four"]));
+    }
+}
